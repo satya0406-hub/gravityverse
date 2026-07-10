6b2ca86f02e390f3c5160812e75fbf626c356fdb
Sathi Satya manikanta Reddy <satyamanikantareddysathi@gmail.com>
2026-07-10 21:30:51 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/gravityverse/',
+    base: "/",
     plugins: [react(), tailwindcss()],
     // Keep other existing configurations unchanged...
     define: {
